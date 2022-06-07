e1f14f4b3de0f7a38d2308fd8248446a60f51982
Chigozirim <iamokwukwe@gmail.com>
2022-06-07 16:56:11 +0100

    Correct error

--- console.py
+++ console.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Implement shell like command line interpreter with python cmd module"""
+"""Defines the AirBnB console."""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """Defines the AirBnB command interpreter."""
     prompt = "hbnb "
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self):
+    def do_EOF(self, arg):
         """Exits the program on receiving end-of-file signal."""
         return True
 
