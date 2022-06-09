2094ed89e424ecb2d5e280656ed68272caf04869
Chigozirim <iamokwukwe@gmail.com>
2022-06-09 17:55:57 +0100

    Update the command interpreter

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 import re
 import cmd
 from shlex import split
+
+from click import argument
 from models import storage
 from models.user import User
 from models.city import City
@@ -47,13 +49,21 @@ class HBNBCommand(cmd.Cmd):
     def default(self, arg):
         """Default behavior for cmd module when input is invalid."""
         cmds = {
-            "all()": self.do_all,
+            "all": self.do_all,
+            "count": self.do_count,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "update": self.do_update,
         }
-        if "." in arg:
+        if "." in arg and "(" in arg and ")" in arg:
             cls = arg[: arg.index(".")]
-            command = arg[arg.index(".") + 1 :]
-            if command in cmds.keys():
-                return cmds[command](cls)
+            method = arg[arg.index(".") + 1 : arg.index("(")]
+            argument = arg[arg.index("(") + 1 : arg.index(")")]
+            argument = "{} {}".format(cls, argument.replace(",", ""))
+            # print(argument)
+            # return
+            if method in cmds.keys():
+                return cmds[method](argument)
         self.stdout.write("*** Unknown syntax: %s\n" % arg)
         return
 
@@ -69,29 +79,29 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Creates a new instance of BaseModel
 
-        Usage: create <cls>
+        Usage: create <class> or <class>.create()
         """
         args = parse(arg)
         print(args[0])
         if len(args) == 0:
-            print("** cls name missing **")
+            print("** class name missing **")
         elif args[0] not in CLASSES:
-            print("** cls doesn't exist **")
+            print("** class doesn't exist **")
         else:
             print(eval(args[0])().id)
             storage.save()
 
     def do_show(self, arg):
-        """Prints the string representation of an instance based on the cls
+        """Prints the string representation of an instance based on the class
         name and id.
 
-        Usage: show <cls> <id>
+        Usage: show <class> <id> or <class>.show()
         """
         args = parse(arg)
         if len(args) == 0:
-            print("** cls name missing **")
+            print("** class name missing **")
         elif args[0] not in CLASSES:
-            print("** cls doesn't exist **")
+            print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
         elif "{}.{}".format(args[0], args[1]) not in storage.all():
@@ -102,14 +112,14 @@ class HBNBCommand(cmd.Cmd):
             print(obj)
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the cls name and id.
-        Usage: destroy <cls> <id>
+        """Deletes an instance based on the class name and id.
+        Usage: destroy <class> <id> or <class>.destroy(<id>)
         """
         args = parse(arg)
         if len(args) == 0:
-            print("** cls name missing **")
+            print("** class name missing **")
         elif args[0] not in CLASSES:
-            print("** cls doesn't exist **")
+            print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
         elif "{}.{}".format(args[0], args[1]) not in storage.all():
@@ -121,16 +131,16 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """Prints all string representation of all instances based or not on
-        the cls name.
+        the class name.
 
-        Usage: all or all <cls>
+        Usage: all or all <class> or <class>.all()
         Ex: (hbnb) all
             (hbnb) all BaseModel
         """
         args = parse(arg)
         if len(args) > 0:
             if args[0] not in CLASSES:
-                print("** cls doesn't exist **")
+                print("** class doesn't exist **")
             else:
                 for key, obj in storage.all().items():
                     if args[0] in key:
@@ -139,18 +149,35 @@ class HBNBCommand(cmd.Cmd):
             for key, obj in storage.all().items():
                 print(obj)
 
-    def do_update(self, arg):
-        """Updates an instance based on the cls name and id by adding
-        or updating attribute.
+    def do_count(self, arg):
+        """Retrieves the number of instances of a class.
 
-        Usage: update <cls name> <id> <attribute name> "<attribute value>"
-        Ex: (hbnb)  update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        Usage: count <class_name> or <class_name>.count()
         """
         args = parse(arg)
         if len(args) == 0:
-            print("** cls name missing **")
+            print("** class name missing **")
         elif args[0] not in CLASSES:
-            print("** cls doesn't exist **")
+            print("** class doesn't exist **")
+        else:
+            count = 0
+            for key in storage.all().keys():
+                if arg[0] in key:
+                    count += 1
+            print(count)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding
+        or updating attribute.
+
+        Usage: update <class name> <id> <attribute name> "<attribute value>" or
+                <class name>.update(<id>, <attribute name>, <attribute value>)
+        """
+        args = parse(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in CLASSES:
+            print("** class doesn't exist **")
         elif len(args) < 2:
             print("** instance id missing **")
         elif "{}.{}".format(args[0], args[1]) not in storage.all():
@@ -173,15 +200,15 @@ class HBNBCommand(cmd.Cmd):
                     # if the attribute already exist, cast the value to the
                     # attribute type.
                     value = args[3]
-                    if args[2] in obj.__cls__.__dict__.keys():
-                        value = type(obj.args[2])(args[3])
+                    if args[2] in obj.__dict__.keys():
+                        value = type(obj.__dict__[args[2]])(args[3])
                     setattr(obj, args[2], value)
-                else:
+                else:  # args[3] is a dictionary
                     for k, v in eval(args[2]).items():
-                        if k in obj.__cls__.__dict__.keys() and type(
-                            obj.__cls__.dict__[k] in {str, int, float}
+                        if k in obj.__dict__.keys() and type(
+                            obj.__class__.__dict__[k] in {str, int, float}
                         ):
-                            valtype = type(obj.__cls__.__dict__[k])
+                            valtype = type(obj.__dict__[k])
                             setattr(obj, k, valtype(v))
                         else:
                             setattr(obj, k, v)
